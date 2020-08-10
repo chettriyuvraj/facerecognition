41eb2e4eba7ed3592ed2532edcd47216512701a8
Yuvraj Chettri <chettriyuvraj@gmail.com>
2020-08-10 19:29:13 +0530

    Updates

---
+++ precache-manifest.353f7fb71cdd7a468fb99be7d903fffc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3059ab4d73b1794accfd7e10bf9c1c9c",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "d80671491bb02a30be79",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3017750fdc2812244eef",
+    "url": "/facerecognition/static/css/main.c5a6d7c3.chunk.css"
+  },
+  {
+    "revision": "d80671491bb02a30be79",
+    "url": "/facerecognition/static/js/2.2c23ba90.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognition/static/js/2.2c23ba90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3017750fdc2812244eef",
+    "url": "/facerecognition/static/js/main.f073c97c.chunk.js"
+  },
+  {
+    "revision": "fa6a11a9cd7eebd80802",
+    "url": "/facerecognition/static/js/runtime-main.1ad3eaac.js"
+  }
+]);
